3b765f2dba785ae3f21790d8792df43b2b04927f
sergey radionov <rsatom@gmail.com>
2022-11-10 23:37:30 +0700

    RAII wrapper added for file descriptors

--- CPtr.h
+++ CPtr.h
@@ -15,3 +15,16 @@ typedef
     std::unique_ptr<
         char,
         CFree> CharPtr;
+
+struct FDAutoClose
+{
+    FDAutoClose(int fd) : fd(fd) {}
+    ~FDAutoClose() { if(fd != -1) close(fd); }
+
+    int get() const { return fd; }
+
+    void cancel() { fd = -1; }
+
+private:
+    int fd;
+};
